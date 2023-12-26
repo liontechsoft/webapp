135c61df3e5d85224e5d14be59e76a4878ebe3b6
AshokkumarString <58688388+AshokkumarString@users.noreply.github.com>
2023-12-26 21:12:41 +0530

    update manikandan screen

--- productionstage/src/App.js
+++ productionstage/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { Basic } from './Screens/Basic';
 
 function App() {
   return (
     <div className="App">
+      <Basic />
       <h1>this is my basic project of react js</h1>
     </div>
   );
---
+++ productionstage/src/Screens/Basic.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Basic=()=>{
+    return (
+        <React.Fragment>
+            <h2>Manikandan</h2>
+        </React.Fragment>
+    )
+}
