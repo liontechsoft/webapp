cc155bf21a8257340740b31cd873fa1a7402bb59
AshokkumarString <154906374+liontechsoft@users.noreply.github.com>
2023-12-29 21:45:14 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Basic />
-      <h1>this is my basic project of react js</h1>
+      <h1>this is my basic project of react js s</h1>
     </div>
   );
 }
